93205527c2cc8b6edd1247f9a3162eb9b7b3c904
kahenteikou <kokkiemouse@gmail.com>
2022-03-21 11:45:22 +0900

    added

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import App from './App';  
+ReactDom.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
