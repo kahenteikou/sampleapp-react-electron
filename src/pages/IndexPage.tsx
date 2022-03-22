74801e66bd446da64fd4f6c963c910832e097071
kahenteikou <kokkiemouse@gmail.com>
2022-03-22 12:41:31 +0900

    ?

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,14 +1,119 @@
 import * as React from "react";
-import Button from '@mui/material/Button';
-
-class IndexPage extends React.Component{
-    public render(): React.ReactNode {
-        return(
-            <>
-                <h1>Index page32</h1>
-                <Button variant="contained">Default</Button>
-            </>
-        );
-    }
+import { Button, Toolbar, Typography, Box, IconButton, CssBaseline, useTheme, styled, Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
 }
-export default IndexPage;
+
+const drawerWidth = 240;
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+const Main2 = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+    open?: boolean;
+  }>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }));
+interface MainProps{
+    open?: boolean;
+    children?: React.ReactNode;
+    id?:string;
+    className?:string;
+}
+const Main=(props:MainProps)=>{
+    return(<>
+        <Main2 open={props.open}>
+            <DrawerHeader />
+            {props.children}
+        </Main2>
+    </>);
+}
+export default function IndexPage() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    }
+    const handleDrawerClose = () => {
+        setOpen(false);
+    }
+    return (<>
+
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Persistent drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left" open={open}>
+                <DrawerHeader>
+                  <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                  </IconButton>
+                </DrawerHeader>
+            </Drawer>
+            <Main open={open}>
+            <h1>Index Page!</h1>
+            </Main>
+        </Box>
+    </>);
+}
